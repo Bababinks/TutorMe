565690a8eda42f038a738deaafca05d7dd79d3ba
Bababinks <bmp5qd@virginia.edu>
2023-02-21 17:16:10 -0500

    Added files and changes for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-@rrwgn5#191&17
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['tutorfinder.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['tutorfinder.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
