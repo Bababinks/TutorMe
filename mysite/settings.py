314e43683a1927c2b8812dbeb4252b4b6191b11f
Bababinks <bmp5qd@virginia.edu>
2023-02-21 19:39:43 -0500

    login features - still needs with with buttons and look

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
+
+    # for oauth
+    'django.contrib.sites',
+    'tutorMe',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -169,3 +178,26 @@ try:
         django_heroku.settings(locals())
 except ImportError:
     found = False
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
+SITE_ID = 3
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- tutorMe/urls.py
+++ tutorMe/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view()),
 
 ]
