c4dac88bfd014ede39cfa3c3343c0dac1eb47e22
ka1979 <kareem.ateeqi@gmail.com>
2023-02-26 14:05:52 -0500

    made changes

--- tutorMe/models.py
+++ tutorMe/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class tutorMeUser(models.Model):
+    email = models.EmailField(unique=True)
+    oauth_uid = models.CharField(max_length=255)
+    is_tutor = models.BooleanField(default=False)
+    is_admin_set = models.BooleanField(default=False)
+
+    # other fields as needed
+
+    def __str__(self):
+        return self.email
