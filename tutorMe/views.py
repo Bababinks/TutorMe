641adcf70af65372eba0bc2d3cc3847ca9b2e1c5
Kyle Pecos <xxn3ap@virginia.edu>
2023-03-08 16:30:47 -0500

    fixed bug

--- tutorMe/views.py
+++ tutorMe/views.py
@@ -92,7 +92,7 @@ def Tutor_Classes_List_View(request):
     class_choice = request.POST.get("class_choice","")
     cur_user = tutorMeUser.objects.get(email=request.user.email)
     if class_choice!="":
-        if not TutorClasses.objects.filter(name=class_choice, tutor_id=cur_user).exists():
+        if not TutorClasses.objects.filter(name=class_choice, tutor=cur_user).exists():
             newclass = TutorClasses();
             newclass.tutor = cur_user
             mnemonic = request.session['0']
@@ -124,6 +124,8 @@ def Tutor_Classes_List_View(request):
 def deleteClass(request,Class):
     cur_user = tutorMeUser.objects.get(email=request.user.email)
 
-    TutorClasses.objects.filter(name=Class, tutor_id=cur_user).delete()
+    namewithoutmneonic = Class.split(' ', 1)[1]
+    query = TutorClasses.objects.filter(name=namewithoutmneonic, tutor_id=cur_user)
+    query.delete()
 
     return Tutor_Classes_List_View(request)
