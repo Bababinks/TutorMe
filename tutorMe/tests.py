4b15f95ac7c298b639cdc2a343a964a02c109f9c
Alisha Meena Gursahaney <118547399+alisha-gursahaney@users.noreply.github.com>
2023-03-14 14:53:29 -0400

    user test

--- tutorMe/tests.py
+++ tutorMe/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import tutorMeUser
 
 # Create your tests here.
+
+class userTest(TestCase):
+
+    def create_user(self):
+        user = tutorMeUser.objects.create()
+        user.save()
+        return user
+
+    def test_user(self):
+        x = self.create_user()
+        self.assertTrue(isinstance(x, tutorMeUser))
+        self.assertEqual(x.is_tutor, False)
