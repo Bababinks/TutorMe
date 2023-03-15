231c4dd07f85a83894ab8323d4faad48158c7b47
Alisha Meena Gursahaney <118547399+alisha-gursahaney@users.noreply.github.com>
2023-03-15 12:47:50 -0400

    commented out a second test

--- tutorMe/tests.py
+++ tutorMe/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from .models import tutorMeUser
+from .views import TutorView, StudentView
+from django.urls import reverse
 
 # Create your tests here.
 
@@ -14,3 +16,15 @@ class userTest(TestCase):
         x = self.create_user()
         self.assertTrue(isinstance(x, tutorMeUser))
         self.assertEqual(x.is_tutor, False)
+
+# class viewsTest(TestCase):
+#
+#     def test_tutorView(self):
+#         # x = self.create_user()
+#         # y = self.client.get(url)
+#         # self.assertEqual(y.status_code, 200)
+#         # self.assertIn(x.title, y.content)
+#         path = reverse("tutorMe:tutor")
+#         request = RequestFactory().get(path)
+#         response = TutorView(request)
+#         assert response.status_code == 200
