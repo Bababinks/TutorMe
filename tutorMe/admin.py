b90cae6fee1007e7167fd745f4150ba0838c1ef0
ka1979 <kareem.ateeqi@gmail.com>
2023-03-15 10:04:36 -0400

    added data

--- tutorMe/admin.py
+++ tutorMe/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import tutorMeUser
+from .models import tutorMeUser,Course
 admin.site.register(tutorMeUser)
+
+
+class MyModelAdmin(admin.ModelAdmin):
+    list_display =["Subject","course_name","course_number"]
+admin.site.register(Course,MyModelAdmin)
+
