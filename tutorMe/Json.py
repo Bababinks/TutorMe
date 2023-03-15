436f669e2941a2ca01c8379375d6b25c2519f585
ka1979 <kareem.ateeqi@gmail.com>
2023-03-15 13:08:15 -0400

    added search function use Searchreds

--- tutorMe/Json.py
+++ tutorMe/Json.py
@@ -1,4 +1,7 @@
 import requests
+from tutorMe.models import Course
+import re
+from difflib import SequenceMatcher
 
 
 def get_JSON_Subjects(year, semester):
@@ -90,4 +93,50 @@ def get_classes(subject_name, year, semester):
 
 
 #print(get_JSON_Subjects("2023", "Spring"))
+
+def Searchereds(keyword):
+    ClassesActual=[]
+    ClassInfo=[]
+    match = re.search(r'\d+', keyword)
+    if match:
+        number_str = match.group()  # extract the number string
+        non_number_str = re.sub(r'\d+', '', keyword).strip()  # remove the number string
+    else:
+        number_str = ''
+        non_number_str = keyword
+    for item in Course.objects.all():
+        ClassInfo=[item.Subject,item.course_name,item.course_number]
+        Sum=0
+        if  not number_str=='':
+            for items in non_number_str.lower().split(" "):
+                if (ClassInfo[0].lower() == items.lower()):
+                    Sum += 1
+                elif (ClassInfo[0].lower() != items.lower()):
+                    Sum+=SequenceMatcher(None, ClassInfo[1].lower(), items.lower()).ratio()
+            if (number_str == ClassInfo[2]):
+                Sum +=1
+            else:
+                Sum += SequenceMatcher(None, number_str, item.course_number).ratio()
+        else:
+            if (keyword.lower() in item.course_name.lower()):
+                Sum += 1
+            else:
+                Sum+=SequenceMatcher(None, keyword, item.course_name.lower()).ratio()
+        rating=Sum
+        ClassInfo.append(number_str)
+        ClassInfo.append(non_number_str)
+        ClassInfo.append(rating)
+        ClassesActual.append(ClassInfo)
+    ClassesActual.sort(key=lambda x: x[-1], reverse=True)
+  # filter out items with a low relevance score
+    if  number_str == '':
+        threshold_score = 0.75
+    else:
+        threshold_score = 1.5
+        # adjust this value to your liking
+    ClassesActual = [course for course in ClassesActual if course[-1] >= threshold_score]
+    if len(ClassesActual)>10:
+        return ClassesActual[:10]
+    else:
+        return  ClassesActual
 
